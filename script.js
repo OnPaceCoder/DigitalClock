cf0aac007df7c40c712c3a93ff6dde4a5f13db14
OnPaceCoder <priyankvasoya7@gmail.com>
2022-06-12 11:38:18 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+setInterval(()=>{
+    var d = new Date();
+ 
+    var hours = d.getHours() > 12 ? 'pm' : 'am';
+    document.querySelector('.clock').innerHTML = ((d.getHours()%12) || 12)  + " : " + d.getMinutes() + " : " + d.getSeconds() + " "+ hours;
+} , 1000)
